72ba8a36d93d26e42febadbfefd1beb464ea66f3
Alek30k <alemank23k@gmail.com>
2024-07-17 16:37:05 -0300

    last commit

--- src/components/ui/custom/Header.jsx
+++ src/components/ui/custom/Header.jsx
@@ -2,7 +2,7 @@ import { Button } from "../button";
 
 const Header = () => {
   return (
-    <div className="p-3 shadow-sm flex justify-between items-centers px-5">
+    <div className="p-3 shadow-sm flex justify-between  items-centers px-5 ">
       <img src="/logo.svg" alt="" />
       <div className="">
         <Button>Sign In</Button>
--- src/components/ui/custom/Hero.jsx
+++ src/components/ui/custom/Hero.jsx
@@ -1,5 +1,14 @@
 const Hero = () => {
-  return <div className="">Hero</div>;
+  return (
+    <div className="flex items-center mx-56 gap-9">
+      <h1 className="font-extrabold text-[50px] text-center">
+        <span className="text-[#f56551]">
+          Discover Your Next Adventure With AI:
+        </span>
+        Personalized Itineraries at Your Fingertips
+      </h1>
+    </div>
+  );
 };
 
 export default Hero;
