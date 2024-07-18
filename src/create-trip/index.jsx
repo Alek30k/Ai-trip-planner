acce07fe4741818ce3efa834ae99e5ef5b7813e2
Alek30k <alemank23k@gmail.com>
2024-07-18 11:19:13 -0300

    last commit

--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,5 +1,18 @@
 const CreateTrip = () => {
-  return <div className="">CreateTrip</div>;
+  return (
+    <div className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10">
+      <h2 className="font-bold text-3xl">Tell us travel preferences</h2>
+      <p className="mt-3 text-gray-500 text-xl">
+        Just provide some basic information, and our trip planner will generate
+        a customized itinerary based on your preferences.
+      </p>
+      <div className="">
+        <div className="">
+          <h2 className=""> What is destination of choice?</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreateTrip;
